d3c985e018ab894c181c76f7873828aa4a88aa33
IgorOdaryuk <odaryukigor@gmail.com>
2023-07-06 22:29:46 +0300

    practice time with swift making easy-entry levels app

---
+++ TicTacToeSwift/TicTacToeSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeSwiftApp.swift
+//  TicTacToeSwift
+//
+//  Created by Igor Odaryuk on 06.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
